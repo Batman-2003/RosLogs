b49123c648a576c2aa600c4b925cee5887e1b2a9
Batman-2003 <g4ur4v03@gmail.com>
2023-08-05 01:28:56 +0530

    added colors for xlsx

--- scripts/transfer_data.py
+++ scripts/transfer_data.py
@@ -3,6 +3,7 @@
 
 ##----------------------------------------------------------------------imports-----------------------------------------------------------------
 import openpyxl
+from openpyxl.styles import Font, colors
 import sys
 import numpy as np
 import math as m
@@ -37,7 +38,12 @@ def add_to_sheet(ind, a0, a1, a2, a3, a4, a5):
     ws['C{_}'.format(_ = ind + 5)] = a2
     ws['D{_}'.format(_ = ind + 5)] = a3
     ws['E{_}'.format(_ = ind + 5)] = a4
-    ws['F{_}'.format(_ = ind + 5)] = a5
+    if a5 == "Colliding":
+        ws['F{_}'.format(_ = ind + 5)] = a5
+        ws['F{_}'.format(_ = ind + 5)].font = Font(color="FF0000")
+    else:
+        ws['F{_}'.format(_ = ind + 5)] = a5
+        ws['F{_}'.format(_ = ind + 5)].font = Font(color="00FF00")
     wb.save("../data_collection/test.xlsx")
 
 
